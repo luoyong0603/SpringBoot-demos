b523e6c454ff16a5dbf21c98e1704d1e7bf31c75
luoyong001 <luoyong0603@foxmail.com>
2023-08-25 17:59:56 +0800

    提交quartz-demo

---
+++ quartz-demo/src/test/java/com/example/quartzdemo/QuartzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartzdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
