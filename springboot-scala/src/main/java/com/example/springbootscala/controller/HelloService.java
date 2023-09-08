41d07301f8142ee14ab59458aeac94c8679e5304
luoyong001 <luoyong0603@foxmail.com>
2023-09-08 15:20:06 +0800

    1

--- springboot-scala/src/main/java/com/example/springbootscala/controller/HelloService.java
+++ springboot-scala/src/main/java/com/example/springbootscala/controller/HelloService.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloService {
 
-    public String sayHello() {
-        Hello hello = new Hello();
-        return hello.sayHello();
-    }
+//    public String sayHello() {
+//        Hello hello = new Hello();
+//        return hello.sayHello();
+//    }
 }
